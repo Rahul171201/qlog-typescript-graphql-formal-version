2e6f2b3dfc8166779cd6491dcc5d2000cb2c8a1d
Rahul Roy (Roy) <rahul.roy@rahul.roy-DM77T2Q225>
2023-03-29 15:27:27 +0530

    sidebar component refactored

--- src/components/Sidebar/SidebarItem/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem/SidebarItem.tsx
@@ -1,8 +1,70 @@
-import styles from "./SidebarItem.module.css";
+import { theme } from '@/reactive-var/theme';
+import { useReactiveVar } from '@apollo/client';
+import styles from './SidebarItem.module.css';
+import { Dispatch, SetStateAction, SyntheticEvent } from 'react';
+import { useContext } from 'react';
+import { SearchContext } from '@/contexts/SearchContext';
 
-const SidebarItem = ({className, value, index} : {className: string, value: string, index: string}) => {
+const SidebarItem = ({
+  currentItem,
+  setCurrentItem,
+  value,
+  index
+}: {
+  currentItem: number;
+  setCurrentItem: Dispatch<SetStateAction<number>>;
+  value: string;
+  index: number;
+}) => {
+  const currentTheme = useReactiveVar(theme);
+
+  // search context
+  const { setSearchText } = useContext(SearchContext);
+
+  const colorTheme = +currentTheme.type[currentTheme.type.length - 1];
+
+  const handleClick = (e: SyntheticEvent) => {
+    const listItem = e.currentTarget.getElementsByTagName('li')[0];
+    const id = listItem.getAttribute('id');
+    if (!id) throw new Error('No attribute of name id found on element');
+
+    const index = +id;
+    const value = listItem.innerText;
+
+    if (setSearchText) {
+      const selectedElement = index;
+      if (currentItem === selectedElement) {
+        setCurrentItem(-1);
+        setSearchText(null);
+        return;
+      } else setCurrentItem(selectedElement);
+      if (value) {
+        const text = value.toLowerCase();
+        setSearchText(text);
+      }
+    }
+  };
+
   return (
-    <li className={`${className} ${styles.listItem}`} id={index}>{value}</li>
+    <div
+      className={
+        currentItem === index
+          ? 'selected' + currentTheme.type
+          : 'notSelected' + currentTheme.type
+      }
+      onClick={handleClick}
+    >
+      <li
+        className={
+          colorTheme === 1
+            ? ` ${styles.listItem} ${styles.theme1}`
+            : ` ${styles.listItem} ${styles.theme2}`
+        }
+        id={`${index}`}
+      >
+        {value}
+      </li>
+    </div>
   );
 };
 
--- src/components/StatsColumn/StatsCard/StatsCard.tsx
+++ src/components/StatsColumn/StatsCard/StatsCard.tsx
@@ -2,12 +2,26 @@ import styles from "./StatsCard.module.css";
 import Link from "next/link";
 import lato from "@/data/latoFont";
 import ImageComponent from "../../ImageComponent/ImageComponent";
+import { useReactiveVar } from "@apollo/client";
+import { theme } from "@/reactive-var/theme";
+import { useEffect, useState } from "react";
+import ThemeType from "@/types/ThemeType";
 
 const StatsCard = ({ id, title, description, attachments } : {id : number, title: string, description: string, attachments: string[]}) => {
+
+  // current theme
+  const currentTheme : ThemeType = useReactiveVar(theme);
+
+  const [className, setClassName] = useState<string>('theme1');
+
+  useEffect(() => {
+    setClassName(currentTheme.type);
+  }, [currentTheme]);
+
   return (
     <Link
       href={"/q/" + id}
-      className={`${styles.cardWrapper} ${lato.className}`}
+      className={className === 'theme1' ? `${styles.cardWrapper} ${lato.className} ${styles.theme1}` : `${styles.cardWrapper} ${lato.className} ${styles.theme2}`}
     >
       <div className={styles.cardTitle}>{title}</div>
       <hr className={styles.horizontalRule}></hr>
